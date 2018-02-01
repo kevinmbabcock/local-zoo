a991da14f60da4ecfbbc29b4b1e4947dd770b0db
Kevin M Babcock <kevinmbabcock@Kevins-MacBook-Air.local>
2018-01-31 21:55:54 -0800

    display master list of animals

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'animal-list',
   template:`
-
+  <ul>
+      <li *ngFor="let currentAnimal of childAnimalList"> {{currentAnimal.name}}
+        <br>
+        <ul>
+          <li>Species: {{currentAnimal.species}}</li>
+          <li>Age: {{currentAnimal.age}}</li>
+          <li>Diet: {{currentAnimal.diet}}</li>
+          <li>Location: {{currentAnimal.location}}</li>
+          <li>Caretakers: {{currentAnimal.caretakers}}</li>
+          <li>Gender: {{currentAnimal.gender}}</li>
+          <li>Likes: {{currentAnimal.likes}}</li>
+          <li>Dislikes: {{currentAnimal.dislikes}}</li>
+        </ul>
+      </li>
+    </ul>
   `
 })
 
 export class AnimalListComponent {
+  @Input() childAnimalList: Animal[];
+  @Output() clickSender = new EventEmitter();
+
 
 }
--- app/app.component.ts
+++ app/app.component.ts
@@ -8,10 +8,17 @@ import { Animal } from './animal.model';
       <div class="jumbotron">
         <h1>Local Neighborhood Zoo</h1>
       </div>
+
+      <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
     </div>
   `
 })
 
 export class AppComponent {
+  masterAnimalList: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud noises')
+  ];
 
 }
